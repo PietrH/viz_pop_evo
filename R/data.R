2afa4ecb8f8aa0bb8fe8d8b7277bebd68e948b18
PietrH <48065851+PietrH@users.noreply.github.com>
2022-09-18 22:12:52 +0200

    document example dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example of population dynamic parameters as provided by users
+#'
+#' A dataset containing reproduction and survival rates, for different species
+#' at different lifestages
+#'
+#' @format A data frame with 10 rows and 5 variables: \describe{
+#'   \item{locality}{species locality}
+#'   \item{species}{species identifying string}
+#'   \item{lifestage}{lifestage}
+#'   \item{reproduction}{reproduction rate}
+#'   \item{survival}{survival rate}}
+"example_pop_dyn"
